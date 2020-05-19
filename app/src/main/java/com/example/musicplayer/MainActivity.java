bf7b0918e0fc9d95200e490dbfe37bb5bc7610a8
neo <pavansantoshls990@gmail.com>
2020-05-19 21:40:38 +0530

    test

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = findViewById(R.id.ViewPager);
+        Initialize();
+
+    }
+
+    private void Initialize(){
+        viewPager = (ViewPager) findViewById(R.id.ViewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        viewPager.setAdapter(new MenuPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT));
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/com/example/musicplayer/MenuPagerAdapter.java
@@ -0,0 +1,51 @@
+package com.example.musicplayer;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MenuPagerAdapter extends FragmentPagerAdapter {
+
+    private int menuItemsCount = 3;
+
+    public MenuPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new PlaylistFragment();
+            case 1:
+                return new SongsFragment();
+            case 2:
+                return new AlbumFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return menuItemsCount;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "Playlists";
+            case 1:
+                return "Songs";
+            case 2:
+                return "Albums";
+            default:
+                return null;
+        }
+    }
+}
