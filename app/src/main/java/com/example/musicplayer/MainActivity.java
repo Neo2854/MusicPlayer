876fd95fa323a55235f222677ea37177b04649b2
neo <pavansantoshls990@gmail.com>
2020-05-21 09:06:31 +0530

    removed test for loop for recycler view

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.musicplayer;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int STORAGE_PERMISSION_CODE = 1;
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     private final int tabIconsCount = 3;
     private int[] tabIcons = {
@@ -25,8 +38,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Initialize();
 
+    }
+
+    private boolean arePermissionsGranted(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            for(int i=0;i<PERMISSIONS.length;i++){
+                if(checkSelfPermission(PERMISSIONS[i]) != PackageManager.PERMISSION_GRANTED){
+                    return false;
+                }
+            }
+        }
+        else {
+            return true;
+        }
+        return true;
+    }
+
+    private void requestStoragePermissions(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(shouldShowRequestPermissionRationale(PERMISSIONS[0]) &&
+                shouldShowRequestPermissionRationale(PERMISSIONS[1])){
+                new AlertDialog.Builder(this)
+                        .setTitle("Permission needed")
+                        .setMessage("Permission needed to read and display songs")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                requestPermissions(PERMISSIONS,STORAGE_PERMISSION_CODE);
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+            }
+            else {
+                requestPermissions(PERMISSIONS,STORAGE_PERMISSION_CODE);
+                if(!arePermissionsGranted()){
+                    requestPermissions(PERMISSIONS,STORAGE_PERMISSION_CODE);
+                }
+            }
+        }
     }
 
     private void Initialize(){
@@ -39,6 +94,22 @@ public class MainActivity extends AppCompatActivity {
         //Setting Tab Icons
         for (int i=0;i<tabIconsCount;i++){
             tabLayout.getTabAt(i).setIcon(tabIcons[i]);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == STORAGE_PERMISSION_CODE){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+               grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                Initialize();
+            }
+            else {
+                Toast.makeText(this,"Permissions are needed",Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
